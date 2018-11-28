f4ca9b55f3d8dae892e1cd613d525751d69b2019
Gürses Armutcu <gursesarmutcu2016@gmail.com>
2018-11-28 12:08:24 +0300

    Initial Commit

---
+++ tek_bagli_dairesel/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tek_bagli_dairesel
+//
+//  Created by Gürses Armutcu on 28.11.2018.
+//  Copyright © 2018 Gürses Armutcu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
